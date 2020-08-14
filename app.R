0f7f91e84290854b0a09e1a38dae308e13a49d4a
BRIANKEN10 <brianken10@gmail.com>
2020-08-14 10:28:52 -0400

    adding existing app work

---
+++ app.R
@@ -0,0 +1,95 @@
+
+library(tidyverse)
+library(here)
+library(leaflet)
+library(sf)
+
+fishcnt.df <- read.csv(paste0(here(),"/data/fish cnt static.csv"))
+
+fishcnt.df$cnt.date <- as.Date(fishcnt.df$Updated)
+
+fishcnt.recent.df <- fishcnt.df %>% 
+  group_by(trap_location) %>% 
+  filter(cnt.date == max(cnt.date))
+
+dam.loc.df <- read.csv(paste0(here(),"/data/dam locations.csv")) %>% 
+  rename(dam = 1)
+
+dam.loc.sf <- dam.loc.df %>% 
+  st_as_sf(.,coords=c("lon","lat"),crs=4326)
+
+me.centroid <- states.temp %>%    
+  sf::st_centroid() %>% 
+  sf::st_coordinates() %>% 
+  as.data.frame() %>% 
+  dplyr::select(lon = X, lat = Y)
+
+
+server <- function(input, output, session){
+  
+  
+  output$dammap <- renderLeaflet({
+    
+    leaflet() %>% 
+      setView(lng = me.centroid$lon, lat = me.centroid$lat,  # making sure map zooms in on the zone
+              zoom = 6) %>% 
+      addProviderTiles(providers$CartoDB,
+                       group = "Base Map") %>% 
+      addCircleMarkers(data = dam.loc.sf %>% 
+                         filter(
+                          !dam == input$trap),  # deselecting the dam
+                       color = "black",
+                       popup = ~dam) %>% 
+      addCircleMarkers(data = dam.loc.sf %>% 
+                         filter(
+                           dam == input$trap),  # deselecting the dam
+                       color = "red",
+                       popup = ~dam)
+  })
+  
+  output$barchart <- renderPlot({
+    
+    trap.filter <- input$trap
+    
+    if(input$noalewife == FALSE){
+      fishcnt.plotinput.df <- fishcnt.recent.df %>% 
+        filter(!spp == "River Herring")
+    }else{fishcnt.plotinput.df <- fishcnt.recent.df}
+    
+    ggplot(data = fishcnt.plotinput.df %>% 
+             filter(trap_location == trap.filter)) + 
+      geom_bar(aes(cnt.date, cnt, fill = spp),
+               stat = "identity") + 
+      labs(title = paste0("Dam Selected: ", trap.filter),
+           x = "Most Recent Count", y = "Fish Count", fill = "Species") + 
+      theme_bw()
+    
+  })
+  
+}
+  
+
+ui <- fluidPage(
+  titlePanel("Very crude sample app with many necessary improvements to be made"),
+  fluidRow(
+    column(
+      width = 4,
+      selectInput(
+                  "trap",
+                  "Dam Location",
+                  c(
+                    as.character(unique(fishcnt.df$trap_location))
+                    )
+      ), # end selector
+      checkboxInput("noalewife", "Include River Herring:", TRUE),
+      plotOutput("barchart")
+    ),  # column  
+    column(
+      width = 8,
+      leafletOutput("dammap")
+    ) # end column
+  )  # end row 
+)
+
+
+shinyApp(ui = ui, server = server)
---
+++ code/scrap-code.R
@@ -0,0 +1,80 @@
+### really quick scrap code fo rapp
+
+library(here)
+library(sf)
+library(tidyverse)
+library(leaflet)
+library(ggmap)
+library(tidygeocoder)
+
+
+fishcnt.df <- read.csv(paste0(here(),"/data/fish cnt static.csv"))
+
+fishcnt.df$cnt.date <- as.Date(fishcnt.df$Updated)
+
+fishcnt.recent.df <- fishcnt.df %>% 
+  group_by(trap_location) %>% 
+  filter(cnt.date == max(cnt.date))
+
+dam.loc.df <- read.csv(paste0(here(),"/data/dam locations.csv"))
+
+dam.loc.sf <- dam.loc.df %>% 
+  st_as_sf(.,coords=c("lon","lat"),crs=4326)
+
+
+gis.data.path <- "C:/Users/brian/OneDrive/Desktop/natural earth data/"
+
+states.temp <- sf::st_read(paste0(gis.data.path, "ESRI_USA_States_Generalized/USA_States_Generalized.shp")) %>% 
+  filter(STATE_NAME == "Maine")
+
+me.centroid <- states.temp %>%    
+  sf::st_centroid() %>% 
+  sf::st_coordinates() %>% 
+  as.data.frame() %>% 
+  dplyr::select(lon = X, lat = Y)
+
+# BASIC MAP 
+
+leaflet() %>% 
+  setView(lng = me.centroid$lon, lat = me.centroid$lat,  # making sure map zooms in on the zone
+          zoom = 6) %>% 
+  addProviderTiles(providers$CartoDB,
+                   group = "Base Map") %>% 
+  addCircleMarkers(data = dam.loc.sf,
+             color = "red",
+            popup = ~dam
+                   )
+
+# BASIC CHART
+
+
+
+if(input$noalewife == FALSE){filter(!spp == "River Herring")}
+
+if(bool == FALSE){x == 2}else{4}
+  
+}
+
+{if (y=="") filter(., x>3) else filter(., x<3)}
+
+
+bool <- FALSE
+
+if(bool == FALSE){
+  fishcnt.plotinput.df <- fishcnt.recent.df %>% 
+    filter(!spp == "River Herring")
+}else{fishcnt.plotinput.df <- fishcnt.recent.df}
+
+ggplot(data = fishcnt.plotinput.df %>% 
+         filter(trap_location == trap.filter))  + 
+  geom_bar(aes(cnt.date, cnt, fill = spp),
+           stat = "identity") + 
+  labs(title = paste0("Dam Selected: ", trap.filter)) + 
+    theme_bw()
+
+
+
+
+
+
+
